acbf563465acd6e87be8763e86c3b119d7b6129c
gh0st1608 <egalindoa@uni.pe>
2026-06-20 14:42:44 -0500

    fix: change app route

--- src/presentation/routes/router.tsx
+++ src/presentation/routes/router.tsx
@@ -1,22 +1,25 @@
-import {
-  createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import { PokemonListScreen } from "@/presentation/screens/pokemon-list-screen";
 import { PokemonDetailScreen } from "@/presentation/screens/pokemon-detail-screen";
 import { MainLayout } from "@/presentation/components/layout/main";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      element: <MainLayout />,
+      children: [
+        {
+          path: "/",
+          element: <PokemonListScreen />,
+        },
+        {
+          path: "/pokemon/:id",
+          element: <PokemonDetailScreen />,
+        },
+      ],
+    },
+  ],
   {
-    element: <MainLayout />,
-    children: [
-      {
-        path: "/",
-        element: <PokemonListScreen />,
-      },
-      {
-        path: "/pokemon/:id",
-        element: <PokemonDetailScreen />,
-      },
-    ],
-  },
-]);
+    basename: "/pokeapp",
+  }
+);
