c14cad30b8231278f2f8838b8af3a8e48462d3dd
gh0st1608 <egalindoa@uni.pe>
2026-06-20 13:18:53 -0500

    fix: change app name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/pokeapp/",
   plugins: [react(), tailwindcss({ optimize : true})],
   resolve: {
     alias: {
